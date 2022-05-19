fd6aa95ca6fd2b0b5c741c40449b318e97a34643
Luanporf <104661314+Luanporf@users.noreply.github.com>
2022-05-19 01:24:16 -0300

    a

--- Exercicios/Curso JS/ex0013/script.js
+++ Exercicios/Curso JS/ex0013/script.js
@@ -1,16 +1,17 @@
 function gerar() {
     let numero = document.getElementById('num')
-    let tabel = document.getElementById('tabela') 
-    if (numeResizeObserver.value.length == 0) {
+    var resultado = document.getElementById('tabuada') 
+
+    if (num.value == 0) {
         alert ('[erro] digite um número FDP')
     }else {
-        let num1 = Number(numero.value)
-        let mul = 1
-        while (mul <= 10) {
-            let item = document.createElement('option');
-            item.text = `${num} X ${mul} = ${num * mul}`;
-            tabel.appendChild(item)
-            mul++
+        let num = Number(numero.value)
+        var mul = 1
+        for (mul;mul <= 10;mul++) {
+            let item = document.createElement('option')
+            item.text = `${num} X ${mul} = ${num*mul}`;
+            resultado.appendChild(item)
+
         }
     }
 }
