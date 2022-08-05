79dcca249a33e43dfd35e032eca9fc815a86770a
Luan Porfirio <104661314+Luanporf@users.noreply.github.com>
2022-08-05 05:10:35 -0300

    Progress!

---
+++ Exercicios/Curso JS/ex0015/função003.js
@@ -0,0 +1,6 @@
+let i = 10
+
+function myFunction () {
+    while (i === 10)
+    return console.log ("Hello,World!")
+  }
---
+++ Exercicios/cursojs rocketseat/Objects.js
@@ -0,0 +1,8 @@
+const person = {
+    nome: 'luan',
+    peso: '72,5 kg',
+    sexo: 'todo dia',
+    tristezas: '0'
+}
+
+console.log (person.tristezas)
